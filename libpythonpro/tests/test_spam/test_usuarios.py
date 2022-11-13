d4054baa254839c31a26cd6b03cc476296086450
gabrielpiazenski@gmail.com <Gp231187!>
2022-11-13 14:52:57 -0300

    isolamento dos testes

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,3 +1,7 @@
+from libpythonpro.tests.spam.db import Conexao
+from libpythonpro.tests.spam.modelos import Usuario
+
+
 def test_salvar_usuario():
     conexao = Conexao()
     sessao = conexao.gerar_sessao()
@@ -14,7 +18,7 @@ def test_listar_usuario():
     usuarios = [Usuario(nome='Gabriel'), Usuario(nome='Renzo')]
     for usuario in usuarios:
         sessao.salvar(usuario)
-    assert usuario == sessao.listar()
+    assert usuarios == sessao.listar()
     sessao.roll_back()
     sessao.fechar()
     conexao.fechar()
