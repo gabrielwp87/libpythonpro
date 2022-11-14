0816dba85781316abe152cf3902b12aba4bbce78
gabrielpiazenski@gmail.com <Gp231187!>
2022-11-14 09:46:53 -0300

    Mock

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,19 +1,10 @@
+from unittest.mock import Mock
+
 import pytest
 
 from libpythonpro.tests.spam.enviador_de_email import Enviador
 from libpythonpro.tests.spam.main import EnviadorDeSpam
 from libpythonpro.tests.spam.modelos import Usuario
-
-
-class EnviadorMock:
-    def __init__(self):
-        super().__init__()
-        self.quantidade_email_enviados = 0
-        self.parametros_de_envio = None
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.quantidade_email_enviados += 1
 
 
 @pytest.mark.parametrize(
@@ -31,27 +22,27 @@ class EnviadorMock:
 def test_quantidade_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'gabrielpiazenski@gmail.com',
         'Curso Python Pro',
         'Confira que estudo fantástico'
     )
-    assert len(usuarios) == enviador.quantidade_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Gabriel', email='gabrielpiazenski@gmail.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'renzo@python.pro.br',
         'Curso Python Pro',
         'Confira que estudo fantástico'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with(
         'renzo@python.pro.br',
         'gabrielpiazenski@gmail.com',
         'Curso Python Pro',
